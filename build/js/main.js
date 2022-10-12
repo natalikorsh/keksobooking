98066a5ccfdd4999e20bacb4e612c4a11504d2d6
Natalia <tonio1811@gmail.com>
2022-10-12 15:27:33 +0200

    создает массив из 10 объектов со случайными наборами ключ-значение

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,5 @@
+import sampleSize from "lodash.samplesize";
+
 const getRandomInteger = (min, max) => {
   if (max === min || max < min) {
     return ('Указан неправильный диапазон');
@@ -17,4 +19,61 @@ const getRandomFloat = (min, max, n) => {
   return +num.toFixed(n);
 }
 
-console.log(getRandomFloat(1.1, 1.2, 5));
+const getAvatarNumber = ({min, max}) => {
+  return '0' + getRandomInteger(min, max).toString();
+}
+
+const AVATAR_NUM = {
+  min: 1,
+  max: 8,
+}
+
+const PICTURES = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
+const TITLES = ['Объявление!', 'Внимание!', 'Важно!', 'Прочтите!'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const TIMES = ['12:00','13:00', '14:00'];
+const DESCRIPTIONS = ['A Hair’s Breadth', 'Goody Two-Shoes', 'Fit as a Fiddle', 'All Greek To Me', 'Hard Pill to Swallow'];
+const getValue = (elements) => {
+  return elements[getRandomInteger(0, TITLES.length - 1)];
+}
+const getRandomArray = (someArray) => {
+  return sampleSize(someArray, getRandomInteger(1, someArray.length));
+}
+
+const createPrint = () => {
+  return {
+    autor: {
+      avatar: 'img/avatars/user' + getAvatarNumber(AVATAR_NUM) + '.png'
+    },
+    
+    offer: {
+      title: getValue(TITLES),
+      address: '{{location.x}}, {{location.y}}',
+      price: getRandomInteger(1, 100),
+      type: getValue(TYPES),
+      rooms: getRandomInteger(1, 5),
+      guests: getRandomInteger(1, 10),
+      checkin: getValue(TIMES),
+      checkout: getValue(TIMES),
+      features: getRandomArray(FEATURES),
+      description: getValue(DESCRIPTIONS),
+      photos: getRandomArray(PICTURES),
+    },
+  
+    location: {
+      x: getRandomFloat(35.65000, 35.70000, 5).toString(),
+      y: getRandomFloat(139.70000, 139.80000, 5).toString(),
+    },
+  };
+};
+
+
+const arrayOfPrints = new Array(1).fill(null).map(() => createPrint());
+
+
+console.log(arrayOfPrints);
